6496c7797f2d304ecf2b19337e952d3427907d44
Abdelfattah <abdelfattah59@gmail.com>
2023-06-18 15:32:50 +0100

    clean code

--- framework/Http/Kernel.php
+++ framework/Http/Kernel.php
@@ -6,39 +6,18 @@ use FastRoute\RouteCollector;
 
 use function FastRoute\simpleDispatcher;
 
-class Kernel{
+class Kernel
+{
     public function handle(Request $request): Response
     {
-        /* $content = "<h1>Hello world</h1>";
-
-        return  new Response(content: $content);
-        */
         // Create  a dispatcher
-        $dispatcher = simpleDispatcher(function(RouteCollector $routeCollector){
-            // STEP 1 
-           /*  $routeCollector->addRoute('GET', '/', function(){
-
-                $content = "<h1>Hello world</h1>";
-
-                return  new Response(content: $content);
-            });
-            $routeCollector->addRoute('GET', '/posts/{id:\d+}', function($routeParams){
-
-                $content = "<h1>this is post : {$routeParams['id']}</h1>";
-
-                return  new Response(content: $content);
-            }); */
-
-            // STEP 2
-            $routes = include BASE_PATH.'/routes/web.php';
+        $dispatcher = simpleDispatcher(function (RouteCollector $routeCollector) {
+            $routes = include BASE_PATH . '/routes/web.php';
 
             foreach ($routes as $route) {
                 $routeCollector->addRoute(...$route);
             }
         });
-
-        //dd($request->getPathInfo());
-       // dd($dispatcher);
 
         // Dispatch a URI, To obtain route info
 
@@ -47,18 +26,9 @@ class Kernel{
             $request->getPathInfo()
         );
 
-        //dd($routeInfo);
-        //step 1
-        //list($status, $handler, $vars) = $routeInfo;
-        //return $handler($vars);
-        
-        // step2
         list($status, list($controller, $method), $vars) = $routeInfo;
-        
+
         // Call the handler, provided by the route info, in order to create a Response
-        // step 1
-        //$response = (new $controller())->$method($vars);
-        // step 2
         $response = call_user_func_array([new $controller, $method], $vars);
         return $response;
     }
--- public/index.php
+++ public/index.php
@@ -1,30 +1,21 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 use KarchalAbdelfattah\Framework\Http\Kernel;
 use KarchalAbdelfattah\Framework\Http\Request;
-use KarchalAbdelfattah\Framework\Http\Response;
 
 define('BASE_PATH', dirname(__DIR__));
 
-require_once dirname(__DIR__).'/vendor/autoload.php';
-//dd(123); // to check if the autoloader work correctly
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
 // request received
 $request = Request::createFromGlobals();
-//dd($request);
 
 // perform some logic
 $kernel = new Kernel();
 
-// send response (string of content)
-//echo "Hello world";
-//step 1
-/* $content = "<h1> Hello world </h1>";
-
-$response = new Response(content: $content, status: 200, headers: []); */
-
-// Step 2
 // call the kernel
-
 $response = $kernel->handle($request);
 
 $response->send();
